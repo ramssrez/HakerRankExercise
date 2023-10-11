fb02536c2f2041b056159d8320ee257467ca9907
Raul Ramirez Perez <ramssrez@gmail.com>
2023-10-11 17:16:30 +0200

    Use the scanner for console

---
+++ src/com/ramssrez/javabasic/StdinStdout.java
@@ -0,0 +1,16 @@
+package com.ramssrez.javabasic;
+
+import java.util.Scanner;
+
+public class StdinStdout {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
