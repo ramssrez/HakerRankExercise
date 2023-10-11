373b19335a75237b80358d8c074706f8cb3d4574
Raul Ramirez Perez <ramssrez@gmail.com>
2023-10-11 17:10:37 +0200

    Create the first challege

---
+++ src/com/ramssrez/javabasic/WelcomeJava.java
@@ -0,0 +1,8 @@
+package com.ramssrez.javabasic;
+
+public class WelcomeJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
